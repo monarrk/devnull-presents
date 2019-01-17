311a09921ec7feeac7b4b7315378fb92502f8244
Skye Bleed <35306846+ithinkthisisarc@users.noreply.github.com>
2019-01-16 21:05:01 -0500

    actual keyboard control

--- main.lua
+++ main.lua
@@ -1,3 +1,41 @@
+-- GLOBAL VARS
+cX = 300
+cY = 300
+
+speed = 2
+sprspeed = 5
+
+ -- MAIN CHUNK
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    if love.keyboard.isDown("w") then
+        if love.keyboard.isDown("lshift") then -- "sprint"
+            cY = cY - sprspeed
+        else
+            cY = cY - speed
+        end
+    end
+    if love.keyboard.isDown("a") then 
+        if love.keyboard.isDown("lshift") then
+            cX = cX - sprspeed
+        else
+            cX = cX - speed
+        end
+    end
+    if love.keyboard.isDown("s") then 
+        if love.keyboard.isDown("lshift") then
+            cY = cY + sprspeed
+        else
+            cY = cY + speed
+        end
+    end
+    if love.keyboard.isDown("d") then
+        if love.keyboard.isDown("lshift") then
+            cX = cX + sprspeed
+        else
+            cX = cX + speed
+        end
+    end
+
+    love.graphics.setColor(255, 0, 0)
+    sprite = love.graphics.circle("fill", cX, cY, 20, 100)
 end
