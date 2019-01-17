4c5f4a23ffd2019c3f045fcaf9da225a133e84e5
Skye Bleed <35306846+ithinkthisisarc@users.noreply.github.com>
2019-01-17 17:51:44 -0500

    bullets

--- main.lua
+++ main.lua
@@ -1,16 +1,51 @@
--- GLOBAL VARS
+-- DEPENDANCIES
+utils =  require( "src/utils" )
 kutils = require( "src/keyboard" )
 
+-- GLOBAL VARS
 cX = 300
 cY = 300
+
+bX = cX
+bY = cY
+
+facing = "n"
+
+bullets = {}
 
 speed = 2
 sprspeed = 5
 
  -- MAIN CHUNK
-function love.draw()
+function love.update()
     kutils.check_keys() -- check for key events
+
+    if love.mouse.isDown(1) then
+        table.insert(bullets, {dire=facing, x=cX, y=cY})
+    end
+end
+
+function love.draw()
+    love.graphics.setBackgroundColor( 255, 255, 255 )
+
+    for i=1, #bullets, 1 do
+        local bullet = bullets[i]
+        love.graphics.setColor(0, 0, 0)
+        if bullet.dire == "n" then
+            bullet.y = bullet.y - 5
+            love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+        elseif bullet.dire == "s" then
+            bullet.y = bullet.y + 5
+            love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+        elseif bullet.dire == "e" then
+            bullet.x = bullet.x + 5
+            love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+        elseif bullet.dire == "w" then
+            bullet.x = bullet.x - 5
+            love.graphics.circle("fill", bullet.x, bullet.y, 7, 100)
+        end
 
     love.graphics.setColor(255, 0, 0)
     sprite = love.graphics.circle("fill", cX, cY, 20, 100)
+    end
 end
