984e19c6e8bae592156591fa12fc44759dd49bc4
Skye Bleed <35306846+ithinkthisisarc@users.noreply.github.com>
2019-01-16 21:12:42 -0500

    clean code

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 -- GLOBAL VARS
+kutils = require( "src/keyboard" )
+
 cX = 300
 cY = 300
 
@@ -7,34 +9,7 @@ sprspeed = 5
 
  -- MAIN CHUNK
 function love.draw()
-    if love.keyboard.isDown("w") then
-        if love.keyboard.isDown("lshift") then -- "sprint"
-            cY = cY - sprspeed
-        else
-            cY = cY - speed
-        end
-    end
-    if love.keyboard.isDown("a") then 
-        if love.keyboard.isDown("lshift") then
-            cX = cX - sprspeed
-        else
-            cX = cX - speed
-        end
-    end
-    if love.keyboard.isDown("s") then 
-        if love.keyboard.isDown("lshift") then
-            cY = cY + sprspeed
-        else
-            cY = cY + speed
-        end
-    end
-    if love.keyboard.isDown("d") then
-        if love.keyboard.isDown("lshift") then
-            cX = cX + sprspeed
-        else
-            cX = cX + speed
-        end
-    end
+    kutils.check_keys() -- check for key events
 
     love.graphics.setColor(255, 0, 0)
     sprite = love.graphics.circle("fill", cX, cY, 20, 100)
